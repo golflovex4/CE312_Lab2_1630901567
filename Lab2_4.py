ac549faf52dff9ef1b68218c1b65427844cf2030
GoRuFu <98442473+golflovex4@users.noreply.github.com>
2022-11-13 22:22:05 +0700

    Create Lab2_4.py

---
+++ Lab2_4.py
@@ -0,0 +1,6 @@
+Array_A= [["Number ID","Name","Count","Status"],]
+Array_LenghtA = 0
+Array_LengthA = len(Array_A)
+Array_A.insert(1,["1","Rubber","0","Out of stock"])
+Array_A.insert(2,["2","Ruler","0","In stock"])
+Array_A.insert(3,["3","Pencil","0","In stock"])
